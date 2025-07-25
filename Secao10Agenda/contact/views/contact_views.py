625341483954df0077da3d8c186c826a8b647639
Arthur A. <arthurakilalves@gmail.com>
2025-07-25 11:12:17 -0300

    Feat: Fazendo a busca do input search buscar dentro do banco de dados e mostrar na tela o valor buscado

--- Secao10Agenda/contact/views/contact_views.py
+++ Secao10Agenda/contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from django.http import Http404
 from contact.models import Contact
 
@@ -44,5 +45,39 @@ def contact(request, contact_id):
     return render(
         request,
         'contatc/contact.html',
+        context
+    )
+
+def search(request):
+    # Request.GET: basicamente pegamos o que o search informa com o request.GET, o valor será um query dict, um dicionario, no qual o valor pesquisado será guardado no valor q (definido no _header.html)
+    # Request.GET.get('q', ''):  basicamente faz com que puxamos uma função do python para pegar o valor q, só que, se ele não achar valor nenhum (alguém apagar a parte do http que mostra o q), ele retorna um valor vazio ''
+    # Request.GET.get('q', '').strip(): uma pessoa pode digitar vários espaços na pesquisa, ex.: '    tutu      ' e colocar para pesquisar, o strip() retira isso
+    search_value = request.GET.get('q', '').strip()
+    
+    # Condicional: se o valor for vázio (mesmo que tenha muitos espaços o strip() já deixou filé pra gente) ele vai redirecionar para o index, tirando assim o valor pesquisado e voltando a url padrão index
+    if search_value == '':
+        return redirect('contact:index')
+
+    # Filtragem com trecho do nome: Imagine que colocamos nico (de nicole, nicolas), queremos que mostre todos os resultados que possuem "nico", utilizando o filter e o __icontains=XY fazemos isso
+    #  Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value): Mas arthur como fariamos para pesquisa nico nos dois nomes? Não simultaneamente tipo Nico (first name) and Nico (last name) como se fosse uma filtragem disso nos dois, mas pesquisasse se tivesse nico em um OU no outro, importamos a ferramenta Q do django que faz a gente utilizar o | (pipe), fazemos a semântica de colocar o Q antes de cada tópico que será verificado e podemos substituir onde seria uma virgula por um | (pipe)
+    contacts = Contact.objects \
+    .filter(show=True) \
+    .filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+        ) \
+    .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
+    }
+
+    return render(
+        request,
+        'contatc/index.html',
         context
     )
