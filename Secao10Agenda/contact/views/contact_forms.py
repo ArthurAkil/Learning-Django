8e55ebea604fc05ddd409110ba7a1c93af99730c
Arthur A. <arthurakilalves@gmail.com>
2025-07-26 10:57:51 -0300

    Feat: Criando forms com o model do django e distinção na view em qual metodo ele está (GET ou POST)

--- Secao10Agenda/contact/views/contact_forms.py
+++ Secao10Agenda/contact/views/contact_forms.py
@@ -4,25 +4,39 @@ from django.http import Http404
 from contact.models import Contact
 from django.core.paginator import Paginator
 
+from django import forms
+
 # Create your views here.
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+            )
+
+
 
 def create(request):
     if request.method == 'POST':
-        print(request.method)
-        print()
-
+        context = {
+            'form': ContactForm(request.POST)
+        }
+        return render(
+            request,
+            'contatc/create.html',
+            context
+        )
         # request.POST.get('first_name'): basicamente estamos entramos no metodo post do request e pegando (get) o valor que possui name igual a string fornecida, ou seja, first_name
-        print(request.POST.get('first_name'))
+        # print(request.POST.get('first_name'))
         
+    # Obs.: Mesmo que tenhamos um form na página não necessariamente está implicito que o metodo é POST, quando acessamos a página o metodo é GET, pois estamos apenas visualizando a página, contudo, quando enviamos algo, o metodo é trocado para POST
+    # print(request.method)
+    # print()
 
     context = {
-
+        'form': ContactForm()
     }
-
-    # Obs.: Mesmo que tenhamos um form na página não necessariamente está implicito que o metodo é POST, quando acessamos a página o metodo é GET, pois estamos apenas visualizando a página, contudo, quando enviamos algo, o metodo é trocado para POST
-    print(request.method)
-    print()
-
     return render(
         request,
         'contatc/create.html',
