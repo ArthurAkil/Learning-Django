21b99c7744c8f4ea908e9e8ebc09821a99fb26a0
Arthur A. <arthurakilalves@gmail.com>
2025-07-30 09:59:06 -0300

    Feat: Criando usuário, reformulando novo forms, adicionando rota, html e view (BETA)

--- Secao10Agenda/contact/forms.py
+++ Secao10Agenda/contact/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -101,4 +102,5 @@ class ContactForm(forms.ModelForm):
         
         return first_name
     
-
+class RegisterForm(UserCreationForm):
+    ...
--- Secao10Agenda/contact/urls.py
+++ Secao10Agenda/contact/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
+
+    # User (CRUD)
+    path('user/register/', views.register, name='register'),
 ]
