def9f213c841f4fb1fb99f849d8be7ec02d398c5
Arthur A. <arthurakilalves@gmail.com>
2025-07-31 09:40:55 -0300

    Feat: Criando pop-ups de confirmação de ação e separando o html no partials

--- Secao10Agenda/contact/views/user_forms.py
+++ Secao10Agenda/contact/views/user_forms.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
+# enviar mensagens para o usuario:
+from django.contrib import messages
 
 def register(request):
 
     form = RegisterForm()
 
+    # mostrar mensagens para o usuário
+    # messages.info(request, 'Um texto qualquer')
+    # messages.success(request, 'Um texto qualquer')
+    # messages.error(request, 'Um texto qualquer')
+    # messages.warning(request, 'Um texto qualquer')
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
+            messages.success(request, 'Usuário registrado')
             form.save()
+            return redirect('contact:index')
             
     return render(
         request,
