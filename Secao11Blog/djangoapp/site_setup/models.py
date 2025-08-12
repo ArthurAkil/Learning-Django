7a238ddd1792c856ad496b3bc76ce1c7b3e9de6f
Arthur A. <arthurakilalves@gmail.com>
2025-08-12 12:21:52 -0300

    feat: adicionando ao models o favicon para o usuario escolher o icone

--- Secao11Blog/djangoapp/site_setup/models.py
+++ Secao11Blog/djangoapp/site_setup/models.py
@@ -32,6 +32,13 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+
+    # icone do site
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m',
+        blank=True,
+        default=''
+    )
     
     def __str__(self):
         return self.title
