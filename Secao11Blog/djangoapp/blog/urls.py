0ff4976910ad547f832f31ff622e37691ce8e847
Arthur A. <arthurakilalves@gmail.com>
2025-08-15 11:24:45 -0300

    feat: modelo de novas views

--- Secao11Blog/djangoapp/blog/urls.py
+++ Secao11Blog/djangoapp/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, page, post
 
 app_name = 'blog'
 
 
 urlpatterns = [
     path('', index,name='index'),
+    path('page/', page, name='page'),
+    path('post/', post, name='post'),
 ]
 
 
--- Secao11Blog/djangoapp/blog/views.py
+++ Secao11Blog/djangoapp/blog/views.py
@@ -6,3 +6,15 @@ def index(request):
     context ={}
 
     return render(request, 'blog/pages/index.html', context)
+
+def page(request):
+
+    context ={}
+
+    return render(request, 'blog/pages/page.html', context)
+
+def post(request):
+
+    context ={}
+
+    return render(request, 'blog/pages/post.html', context)
