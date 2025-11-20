d40937d62a94e3a234f2f2d986ca23e52124b752
Arthur A. <arthurakilalves@gmail.com>
2025-11-20 20:27:17 -0300

    feat: adicionando urls dos arquivos específicos

--- Curso_3_DRF/cursos/urls.py
+++ Curso_3_DRF/cursos/urls.py
@@ -1,11 +1,16 @@
 # Define as rotas (URLs) da sua API e liga cada rota à view correspondente
 
 from django.urls import path 
-from .views import CursoAPIView, AvaliacaoAPIView
+# from .views import CursoAPIView, AvaliacaoAPIView
+from .views.curso_views import CursoAPIView, CursoDetailAPIView
+from .views.avaliacao_views import AvaliacaoAPIView, AvaliacaoDetailAPIView 
 
 urlpatterns = [
+    # Cursos:
     path('cursos/', CursoAPIView.as_view(), name='cursos'), 
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes')
+    path('cursos/<int:id>/', CursoDetailAPIView.as_view(), name='curso'), 
+    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:id>/', AvaliacaoDetailAPIView.as_view(), name="avaliacao")
 ]
 
 # as_view(): Converte a classe CursoAPIView em uma view “funcional”.
